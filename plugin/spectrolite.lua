758c81e5f611455ca9d4cf0edcb79e401d618c85
Artem Shush <shushtain@gmail.com>
2025-04-16 16:38:58 +0300

    No-space commands

--- plugin/spectrolite.lua
+++ plugin/spectrolite.lua
@@ -8,18 +8,18 @@ vim.g.loaded_spectrolite = 1
 
 -- user commands
 
-vim.api.nvim_create_user_command("Spectrolite hex", function()
+vim.api.nvim_create_user_command("SpectroliteHex", function()
 	require("spectrolite").to_hex()
 end, { range = true })
 
-vim.api.nvim_create_user_command("Spectrolite rgb", function()
+vim.api.nvim_create_user_command("SpectroliteRgb", function()
 	require("spectrolite").to_rgb()
 end, { range = true })
 
-vim.api.nvim_create_user_command("Spectrolite hsl", function()
+vim.api.nvim_create_user_command("SpectroliteHsl", function()
 	require("spectrolite").to_hsl()
 end, { range = true })
 
-vim.api.nvim_create_user_command("Spectrolite hxl", function()
+vim.api.nvim_create_user_command("SpectroliteHxl", function()
 	require("spectrolite").to_hxl()
 end, { range = true })
